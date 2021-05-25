367abc3e686c34c83aa41d1cb20c8efa8650cc56
EL2020 <el2020202020@gmail.com>
2021-05-25 00:25:19 -0400

    initial commit

---
+++ postload.js
@@ -0,0 +1,37 @@
+window.foodAPI = {
+
+    foodList: {},
+    spriteSrc: {},
+
+
+
+    /**
+     * Registers a new food entry.
+     * @param modID {String} The internal ID of the mod registering items.
+     * @param imgPath {String} The path to the food spritesheet. The path media/entity/player/foods/ is automatically applied.
+     * @param itemList {Array<String>} An array of strings for food sprite IDs.
+     **/
+    registerFoods(modID, imgPath, itemList){
+        // do not allow two mods attempt to register the same id twice
+        if (modID in this.spriteSrc) throw new Error(`FoodAPI: Food registry with ID ${modID} already registered!`)
+        
+        // gets the proper location of the food tilesheet
+        const updatedImagePath = imgPath.match(/^media\/entity\/player\/foods\//) == null ? "media/entity/player/foods/" + imgPath : imgPath
+        let img = new ig.TileSheet(updatedImagePath, 16, 16, 0, 0)
+        if (img.image.width % 16 !== 0) throw new Error(`FoodAPI: Food tilesheet at ${updatedImagePath} should have a pixel width with a multiple of 16.`)
+        foodAPI.spriteSrc[modID] = img;
+
+        let itemID = ""
+        for(let i = 0; i < itemList.length; i++){
+            itemID = itemList[i]
+            if(itemID in foodAPI.foodList){
+                console.error(`FoodAPI: Item with name ${itemID} already registered by ${foodAPI.foodList[itemID].src}`);
+                continue;
+            }
+            foodAPI.foodList[itemID] = {
+                src: modID,
+                index: i
+            }
+        }
+    }
+}
---
+++ prestart.js
@@ -0,0 +1,71 @@
+ig.ACTION_STEP.SHOW_FOOD_ICON.inject({
+    init(a) {
+        this.customItem = a.icon in foodAPI.foodList
+        this.icon = sc.FOOD_SPRITE[a.icon] ?? (this.customItem ? a.icon : 0);
+        this.offset = a.offset || null;
+    },
+    start(a) {
+        a = ig.game.spawnEntity(sc.FoodIconEntity, 0, 0, 0, {
+            icon: this.icon,
+            customItem: this.customItem,
+            combatant: a
+        });
+        this.offset && a.setState(sc.FOOD_ICON_STATE.HOLD, this.offset)
+    }
+})
+
+
+sc.FoodIconEntity.inject({
+    customItem: false,
+    
+    init: function(a, b, d, g) {
+        this.parent(a, b, d, g);
+        this.customItem = g.customItem;
+    },
+
+    updateSprites() {
+        var d = {};
+        var a = this.timer / 0.1;
+        this.state != sc.FOOD_ICON_STATE.DONE && (a = 1 - a);
+        var b, f = 0, g = 0;
+        if (this.state != sc.FOOD_ICON_STATE.HOLD) {
+            this.setSpriteCount(2, true);
+            b = this.sprites[0];
+            b.setPos(this.coll.pos.x - 12 + this.offset.x, this.coll.pos.y + 2, this.coll.pos.z + 28 + 2 + this.offset.y);
+            b.setSize(24, 0, 32);
+            b.setPivot(12, 16);
+            b.setImageSrc(this.bubbleGfx, 0, 288);
+            b.setAlpha(0.8);
+            b.setTransform(a, a, 0);
+            b = this.sprites[1];
+            f = 0;
+            g = 38
+        } else {
+            this.setSpriteCount(1);
+            b = this.sprites[0];
+            f = -10;
+            g = 17
+        }
+        f = f + this.offset.x;
+        g = g + this.offset.y;
+        b.setPos(this.coll.pos.x + f - 8, this.coll.pos.y + 4, this.coll.pos.z + g + 4);
+        b.setSize(16, 0, 16);
+        b.setPivot(8, 8);
+        if(!this.customItem)
+        {
+            f = this.foodSheet.getTileSrc(d, this.icon);
+            b.setImageSrc(this.foodSheet.image, f.x, f.y);
+        }
+        else
+        {
+            let customFood = foodAPI.foodList[this.icon]
+            let foodSrc = foodAPI.spriteSrc[customFood.src];
+            let imgWidth = foodSrc.image.width / 16
+
+            let offsetX = (customFood.index % imgWidth) * 16,
+                offsetY = (Math.floor(customFood.index / imgWidth)) * 16
+            b.setImageSrc(foodSrc.image, offsetX, offsetY)
+        }
+        b.setTransform(a, a, 0)
+    }
+})

ecf7416157d334fe7a2ef82a5a05edd5d44087c4
EL2020 <el2020202020@gmail.com>
2021-05-25 09:36:43 -0400

    heavily reduced unneeded code in FoodIconEntity.updateSprites()

--- prestart.js
+++ prestart.js
@@ -24,48 +24,18 @@ sc.FoodIconEntity.inject({
     },
 
     updateSprites() {
-        var d = {};
-        var a = this.timer / 0.1;
-        this.state != sc.FOOD_ICON_STATE.DONE && (a = 1 - a);
-        var b, f = 0, g = 0;
-        if (this.state != sc.FOOD_ICON_STATE.HOLD) {
-            this.setSpriteCount(2, true);
-            b = this.sprites[0];
-            b.setPos(this.coll.pos.x - 12 + this.offset.x, this.coll.pos.y + 2, this.coll.pos.z + 28 + 2 + this.offset.y);
-            b.setSize(24, 0, 32);
-            b.setPivot(12, 16);
-            b.setImageSrc(this.bubbleGfx, 0, 288);
-            b.setAlpha(0.8);
-            b.setTransform(a, a, 0);
-            b = this.sprites[1];
-            f = 0;
-            g = 38
-        } else {
-            this.setSpriteCount(1);
-            b = this.sprites[0];
-            f = -10;
-            g = 17
-        }
-        f = f + this.offset.x;
-        g = g + this.offset.y;
-        b.setPos(this.coll.pos.x + f - 8, this.coll.pos.y + 4, this.coll.pos.z + g + 4);
-        b.setSize(16, 0, 16);
-        b.setPivot(8, 8);
-        if(!this.customItem)
-        {
-            f = this.foodSheet.getTileSrc(d, this.icon);
-            b.setImageSrc(this.foodSheet.image, f.x, f.y);
-        }
-        else
+        this.parent()
+        var b = this.sprites[(this.state != sc.FOOD_ICON_STATE.HOLD) ? 1 : 0]  
+        
+        if(this.customItem)
         {
             let customFood = foodAPI.foodList[this.icon]
             let foodSrc = foodAPI.spriteSrc[customFood.src];
-            let imgWidth = foodSrc.image.width / 16
-
+            let imgWidth = Math.floor(foodSrc.image.width / 16)
+    
             let offsetX = (customFood.index % imgWidth) * 16,
                 offsetY = (Math.floor(customFood.index / imgWidth)) * 16
             b.setImageSrc(foodSrc.image, offsetX, offsetY)
         }
-        b.setTransform(a, a, 0)
     }
 })
